09660e8cab7c24d70e71e541584697b779cc2aab
Tiago Amorim <tiago.andre.amorim@gmail.com>
2024-02-15 00:44:03 +0000

    updated migrations and corresponding metadata

--- migrations/001_t_client_orders.sql
+++ migrations/001_t_client_orders.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS client_orders (
-  order_number bigint NOT NULL primary key,
-  client_name_id varchar NOT NULL,
-  work_piece varchar NOT NULL,
-  quantity int NOT NULL,
-  due_date int NOT NULL,
-  late_pen money NOT NULL,
-  early_pen money NOT NULL
+  client_name_id  varchar       ,
+  order_number    bigint        ,
+  work_piece      varchar       NOT NULL,
+  quantity        int           NOT NULL,
+  due_date        int           NOT NULL,
+  late_pen        money         NOT NULL,
+  early_pen       money         NOT NULL,
+
+  PRIMARY KEY (client_name_id, order_number)
 );
--- migrations/002_t_supplier_data.sql
+++ migrations/002_t_supplier_data.sql
@@ -1,32 +1,33 @@
 CREATE TABLE IF NOT EXISTS suppliers (
-  supplier_id varchar NOT NULL primary key,
-  name varchar NOT NULL,
-  piece varchar NOT NULL,
-  min_order int NOT NULL,
-  price_per_piece money NOT NULL,
-  delivery_time interval NOT NULL
+  name             varchar  ,
+  piece            varchar  ,
+  min_order        int      NOT NULL,
+  price_per_piece  money    NOT NULL,
+  delivery_time    interval NOT NULL,
+
+  PRIMARY KEY (name, piece)
 );
 
 INSERT INTO suppliers (
-  supplier_id, name, piece, min_order, price_per_piece, delivery_time
-) VALUES ('A_P1', 'Supplier A', 'P1', 16, '$30', '4 days');
+  name, piece, min_order, price_per_piece, delivery_time
+) VALUES ('Supplier A', 'P1', 16, '$30', '4 days');
 
 INSERT INTO suppliers (
-  supplier_id, name, piece, min_order, price_per_piece, delivery_time
-) VALUES ('A_P2', 'Supplier A', 'P2', 16, '$10', '4 days');
+  name, piece, min_order, price_per_piece, delivery_time
+) VALUES ('Supplier A', 'P2', 16, '$10', '4 days');
 
 INSERT INTO suppliers (
-  supplier_id, name, piece, min_order, price_per_piece, delivery_time
-) VALUES ('B_P1', 'Supplier B', 'P1', 8, '$45', '2 days');
+  name, piece, min_order, price_per_piece, delivery_time
+) VALUES ('Supplier B', 'P1', 8, '$45', '2 days');
 
 INSERT INTO suppliers (
-  supplier_id, name, piece, min_order, price_per_piece, delivery_time
-) VALUES ('B_P2', 'Supplier B', 'P2', 8, '$15', '2 days');
+  name, piece, min_order, price_per_piece, delivery_time
+) VALUES ('Supplier B', 'P2', 8, '$15', '2 days');
 
 INSERT INTO suppliers (
-  supplier_id, name, piece, min_order, price_per_piece, delivery_time
-) VALUES ('C_P1', 'Supplier C', 'P1', 4, '$55', '1 days');
+  name, piece, min_order, price_per_piece, delivery_time
+) VALUES ('Supplier C', 'P1', 4, '$55', '1 days');
 
 INSERT INTO suppliers (
-  supplier_id, name, piece, min_order, price_per_piece, delivery_time
-) VALUES ('C_P2', 'Supplier C', 'P2', 4, '$18', '1 days');
+  name, piece, min_order, price_per_piece, delivery_time
+) VALUES ('Supplier C', 'P2', 4, '$18', '1 days');
