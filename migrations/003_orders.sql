fb725d84967d963e9de503a2f3a7f04c5130e014
Tiago Amorim <tiago.andre.amorim@gmail.com>
2024-03-07 22:30:22 +0000

    reordered due to missing keys on previous version

--- migrations/003_orders.sql
+++ migrations/003_orders.sql
@@ -1,11 +1,15 @@
 CREATE TABLE IF NOT EXISTS orders (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
   client_id uuid NOT NULL REFERENCES clients(id),
-  placement_day int NOT NULL,
-  number int NOT NULL CHECK (due_date > 0),
+  number int NOT NULL UNIQUE CHECK (number> 0),
+  piece char(2) NOT NULL REFERENCES pieces(code),
+  quantity int NOT NULL CHECK (quantity > 0),
   due_date int NOT NULL CHECK (due_date > 0),
+  late_penalty money NOT NULL,
   early_penalty money NOT NULL,
-  late_penalty money NOT NULL
+
+  placement_day int NOT NULL
 );
 
 
