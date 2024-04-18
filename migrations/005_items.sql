f8711018569991ee5eed5d8c8c8cb476549b12fd
Tiago Amorim <tiago.andre.amorim@gmail.com>
2024-04-18 13:44:37 +0100

    fixed bug where check_if_order_is_completed trigger was not working properly

--- migrations/005_items.sql
+++ migrations/005_items.sql
@@ -18,32 +18,35 @@ CREATE TABLE IF NOT EXISTS items (
   CHECK (( status = 'delivered' AND order_id IS NOT NULL) OR (status <> 'delivered'))
 );
 
-CREATE FUNCTION check_if_order_is_completed() RETURNS TRIGGER AS $$
-DECLARE n_ready int;
-DECLARE eq_order RECORD;
-BEGIN
-  SELECT * INTO eq_order FROM orders WHERE orders.id = NEW.order_id;
+CREATE FUNCTION check_if_order_is_completed()
+RETURNS TRIGGER AS $$
+  DECLARE n_ready int;
+  DECLARE eq_order RECORD;
+  BEGIN
+    SELECT * INTO eq_order FROM orders WHERE orders.id = NEW.order_id;
 
-  IF NEW.piece_kind = eq_order.piece THEN
+    IF NEW.piece_kind = eq_order.piece THEN
+      SELECT COUNT(items.*) INTO n_ready FROM items
+      WHERE items.order_id = eq_order.id
+        AND items.piece_kind = eq_order.piece
+        AND items.status = 'in_stock';
 
-    SELECT COUNT(items.*) INTO n_ready FROM items
-    WHERE items.order_id = eq_order.id
-      AND items.piece_kind = eq_order.piece
-      AND items.status = 'in_stock';
+      IF n_ready = eq_order.quantity THEN
+        UPDATE orders
+        SET status = 'completed'
+        WHERE id = eq_order.id;
 
-    IF n_ready = orders.quantity THEN
-      UPDATE orders
-      SET status = 'completed'
-      WHERE id = eq_order.id;
+        RAISE NOTICE 'Order % is completed', eq_order.id;
+      END IF;
     END IF;
 
-  END IF;
-
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+    RETURN NEW;
+  END;
+$$
+LANGUAGE plpgsql;
 
 CREATE TRIGGER check_if_order_is_completed
 AFTER UPDATE OF status ON items
+FOR EACH ROW
 EXECUTE FUNCTION check_if_order_is_completed();
 
